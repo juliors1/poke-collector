d5943b4784f8dc64e1e42184ec744fddc91235d3
Juliors1 <user@email.com>
2020-12-14 12:42:36 -0500

    fix  profile and set up pokemon team tab

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", function (req, res) {
-  res.render("index", { title: "Home Page", user: req.user ? req.user : null });
+  res.render("index", { title: "Poke Collector", user: req.user ? req.user : null });
 });
 
 module.exports = router;
