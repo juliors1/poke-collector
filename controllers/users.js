96f04b5910e6b4ab75aa9ff873cffb8bca369482
Juliors1 <user@email.com>
2020-12-14 16:02:43 -0500

    update function

--- controllers/users.js
+++ controllers/users.js
@@ -4,23 +4,23 @@ module.exports = {
   index,
   showProfile,
   update,
-
 };
 
 function index(req, res) {
   User.find({}).then((users) => {
-    res.render("users/index", { title: "User index", user: req.user, users });
+    res.render("users/index", { title: "Trainer List", user: req.user, users });
   });
 }
-function showProfile(req,res){
+function showProfile(req, res) {
   User.findById(req.user._id)
-  .then((user)=>{
-    res.render('users/profile', {title:"My Profile", user})
-  })
+  .populate("friends")
+  .then((user) => {
+    res.render("users/profile", { title: "My Profile", user });
+  });
 }
-function update(req,res){
-  User.findByIdAndUpdate(req.user._id, req.body, {new:true})
-  .then(()=>{
-    res.redirect("/users/profile")
-  })
+function update(req, res) {
+  User.findByIdAndUpdate(req.user._id, req.body, { new: true })
+  .then(() => {
+    res.redirect("/users/profile");
+  });
 }
