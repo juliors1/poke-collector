54fac0eb000f0b175d501965d5271b97df7b2ffa
Juliors1 <user@email.com>
2020-12-17 12:41:38 -0500

    set up pokemon router in server

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ require("./config/passport");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const authRouter = require("./routes/auth");
+const pokemonRouter = require('./routes/pokemons')
 
 const app = express();
 
@@ -50,6 +51,7 @@ app.use(passport.session());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
+app.use("/pokemon", pokemonRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
