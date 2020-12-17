33199c15acedd6b915522023d7ac087fab556ff5
Juliors1 <user@email.com>
2020-12-17 13:33:14 -0500

    create pokedex page and have it render

--- controllers/pokemons.js
+++ controllers/pokemons.js
@@ -1,7 +1,14 @@
-const Pokemon = require('../models/pokemon')
-const axios = require('axios')
-
+const Pokemon = require("../models/pokemon");
+const axios = require("axios");
 
 module.exports = {
-  
+  pokedex,
+};
+
+function pokedex(req, res) {
+  res.render("pokemon/pokedex", {
+    title: "Pokedex",
+    pokemon: null,
+    user: req.user ? req.user : null,
+  });
 }
