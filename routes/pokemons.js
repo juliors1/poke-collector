18c9fc4e4e550a8f069054adcfb293ff281f5263
Juliors1 <user@email.com>
2020-12-18 01:02:13 -0500

    create router for delete

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -4,6 +4,7 @@ const pokemonCtrl = require("../controllers/pokemons");
 router.get('/pokedex',isLoggedIn, pokemonCtrl.pokedex)
 router.post('/search', isLoggedIn, pokemonCtrl.search);
 router.post('/:id/team', isLoggedIn, pokemonCtrl.add)
+router.delete('/:id/team',isLoggedIn,pokemonCtrl.delete)
 
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next();
